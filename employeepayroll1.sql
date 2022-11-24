80b74ef9517872fa0ee1879c41b8305d16f10fe7
PREETI2096 <preetirg96@gmail.com>
2022-11-24 23:49:12 +0530

    retrive employee payroll data

---
+++ employeepayroll1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE employeepayroll1;
+USE employeepayroll1;
+show tables;
+CREATE TABLE employeepayroll1(
+id int auto_increment,
+name varchar(20),
+gender varchar(10),
+city varchar(30),
+Salary float,
+StartDate Date,
+primary key(id)
+);
+desc employeepayroll1;
+insert into employeepayroll1 (name,gender,city,Salary,StartDate) values('Tushar','male','Bidar',40000,'2022-11-21'),('Sanket','male','Hariyana',30000,'2022-11-21'),('Avinash','male','Kolapur',60000,'2022-11-21');
+insert into employeepayroll1 (name,gender,city,Salary,StartDate) values('Preeti','female','Dharwad',60000,'2022-11-21');
+drop table  employeepayroll1;
+SELECT * FROM employeepayroll1;
