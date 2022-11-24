52e74ebc6aa5af825b2994b6f7979a297cf3df0c
PREETI2096 <preetirg96@gmail.com>
2022-11-24 21:58:29 +0530

    create table

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -1,1 +1,10 @@
 create database employeepayroll1;
+use employeepayroll1 ;
+show tables;
+create table employee (
+id int auto_increment,
+name varchar(20),
+gender varchar(10),
+city varchar(30),
+primary key(id)
+);
