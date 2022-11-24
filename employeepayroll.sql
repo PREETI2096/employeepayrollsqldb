841eea22b30cfe915d1901656c8a8a0f9ae47abd
PREETI2096 <preetirg96@gmail.com>
2022-11-24 23:20:01 +0530

    create employee payroll data

--- employeepayroll.sql
+++ employeepayroll.sql
@@ -8,3 +8,6 @@ gender varchar(10),
 city varchar(30),
 primary key(id)
 );
+insert into employee(name,gender,city) values("Nagraj","male","Bidar"),("Manoj","male","Hariyana"),("Prem","male","Kolapur");
+insert into employee(name,gender,city) values("Preeti","female","Dharwad");
+SELECT * FROM employee;
